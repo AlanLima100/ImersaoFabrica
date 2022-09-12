a4fa80680a2d2c1fc955a29fe00349e219a53c1f
AlanLima100 <111168245+AlanLima100@users.noreply.github.com>
2022-09-12 17:57:09 -0300

    Add files via upload

---
+++ modulo.py
@@ -0,0 +1,12 @@
+def multiplica(a, b):
+    return a*b
+
+def soma(a, b):
+    return a+b
+
+def diminui(a,b):
+    return a-b
+
+def divide(a,b):
+    return a/b
+    
---
+++ testes.py
@@ -0,0 +1,9 @@
+from modulo import multiplica, soma, diminui, divide
+
+print(multiplica(6,9))
+
+print(soma(2,9))
+
+print(diminui(21,10))
+
+print(divide(28,9))
